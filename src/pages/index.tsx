774047760f49a5e2b5e4dbfd25f4c8f26145a73b
Athma Chidambaravasi <athma.vasi@protonmail.com>
2023-01-06 13:45:54 -0700

    populating logic for getRandKey func

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,21 +3,222 @@ import gradient from "@privjs/gradients";
 import { v4 as uuidv4 } from "uuid";
 import random from "random";
 import { AiOutlineCopy } from "react-icons/ai";
+import { useState } from "react";
+
+type Chars = {
+  upperCase: string;
+  lowerCase: string;
+  number: string;
+  symbol: string;
+};
+
+type GetKeyMap = Map<number, (chars: Chars) => string>;
+
+type FormMapObj = keyof Chars | "charLength";
 
 const Home: NextPage = () => {
-  // const gradientBG = gradient(uuidv4());
+  const [formMapObj, setFormMapObj] = useState<Map<FormMapObj, string>>(
+    new Map()
+  );
+  const [charLength, setCharLength] = useState("");
+  const [generatedPassword, setGeneratedPassword] = useState("");
 
-  const randNum = random.int(0, 9);
-  console.log(randNum);
+  const chars = {
+    upperCase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    lowerCase: "abcdefghijklmnopqrstuvwxyz",
+    number: "0123456789",
+    symbol: "#$%^&*()_+~\\`|}{[]:;?><,./-=",
+  };
+
+  /*
+  const getKey = [
+    function upperCase(chars: Chars) {
+      const length = chars.upperCase.length;
+      return chars.upperCase[random.int(0, length - 1)] ?? "";
+    },
+    function lowerCase(chars: Chars) {
+      const length = chars.lowerCase.length;
+      return chars.lowerCase[random.int(0, length - 1)] ?? "";
+    },
+    function number(chars: Chars) {
+      const length = chars.number.length;
+      return chars.number[random.int(0, length - 1)] ?? "";
+    },
+    function symbol(chars: Chars) {
+      const length = chars.symbol.length;
+      return chars.symbol[random.int(0, length - 1)] ?? "";
+    },
+  ];
+*/
+
+  const getKeyMap: GetKeyMap = new Map([
+    [
+      0,
+      function upperCase(chars: Chars) {
+        const length = chars.upperCase.length;
+        return chars.upperCase[random.int(0, length - 1)] ?? "";
+      },
+    ],
+    [
+      1,
+      function lowerCase(chars: Chars) {
+        const length = chars.lowerCase.length;
+        return chars.lowerCase[random.int(0, length - 1)] ?? "";
+      },
+    ],
+    [
+      2,
+      function number(chars: Chars) {
+        const length = chars.number.length;
+        return chars.number[random.int(0, length - 1)] ?? "";
+      },
+    ],
+    [
+      3,
+      function symbol(chars: Chars) {
+        const length = chars.symbol.length;
+        return chars.symbol[random.int(0, length - 1)] ?? "";
+      },
+    ],
+  ]);
+
+  function getRandKey(
+    getKeyMap: GetKeyMap,
+    chars: Chars,
+    formMapObj: Map<FormMapObj, string>
+  ) {
+    const size = getKeyMap.size;
+
+    //uppercase only selected
+    if (
+      formMapObj.get("lowerCase") ??
+      formMapObj.get("number") ??
+      formMapObj.get("symbol") ??
+      formMapObj.get("upperCase")
+    ) {
+      // const randInt = random.int(0, size - 1);
+      const func = getKeyMap.get(1);
+
+      if (func !== undefined) return func(chars);
+    }
+
+    //lowercase only selected
+    if (
+      formMapObj.get("upperCase") ??
+      formMapObj.get("number") ??
+      formMapObj.get("symbol") ??
+      formMapObj.get("lowerCase")
+    ) {
+      const func = getKeyMap.get(1);
+
+      if (func !== undefined) return func(chars);
+    }
+
+    //numbers only selected
+    if (
+      formMapObj.get("upperCase") ??
+      formMapObj.get("lowerCase") ??
+      formMapObj.get("symbol") ??
+      formMapObj.get("number")
+    ) {
+      const func = getKeyMap.get(2);
+
+      if (func !== undefined) return func(chars);
+    }
+
+    //symbols only selected
+    if (
+      formMapObj.get("upperCase") ??
+      formMapObj.get("lowerCase") ??
+      formMapObj.get("number") ??
+      formMapObj.get("symbol")
+    ) {
+      const func = getKeyMap.get(3);
+
+      if (func !== undefined) return func(chars);
+    }
+
+    //uppercase and lowercase only
+    if (
+      formMapObj.get("number") ??
+      formMapObj.get("symbol") ??
+      (formMapObj.get("upperCase") && formMapObj.get("lowerCase"))
+    ) {
+      const randInt = random.int(0, 1);
+      const randFunc = getKeyMap.get(randInt);
+
+      if (randFunc !== undefined) return randFunc(chars);
+    }
+
+    //uppercase, lowercase and numbers only
+    if (
+      formMapObj.get("symbol") ??
+      (formMapObj.get("upperCase") &&
+        formMapObj.get("lowerCase") &&
+        formMapObj.get("number"))
+    ) {
+      const randInt = random.int(0, 2);
+      const randFunc = getKeyMap.get(randInt);
+
+      if (randFunc !== undefined) return randFunc(chars);
+    }
+
+    //uppercase, numbers and symbols only
+    //uppercase and numbers only
+    //uppercase and symbols only
+
+    //lowercase, numbers and symbols only
+    //lowercase and numbers only
+    //
+  }
+
+  function populatePasswordAndSetState(
+    getRandKey: (
+      getKeyMap: GetKeyMap,
+      chars: Chars,
+      formMapObj: Map<FormMapObj, string>
+    ) => string | undefined
+  ) {
+    let generatedPass = "";
+    console.log(getRandKey(getKeyMap, chars, formMapObj));
+
+    while (generatedPass.length < Number(charLength)) {
+      generatedPass += getRandKey(getKeyMap, chars, formMapObj) ?? "";
+      console.log("while loop entered");
+
+      // generatedPass = `${generatedPass}${getRandKey(getKeyMap, chars, formMapObj) ?? ""}`;
+    }
+
+    console.log(generatedPass);
+    // setGeneratedPassword(generatedPass);
+    // console.log(getRandKey(getKeyMap, chars, formMapObj));
+  }
 
   function handleFormSubmit(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
+
+    const formData = new FormData(event.currentTarget);
+    const formMap = [...formData.entries()].reduce(
+      (
+        mapObj: Map<FormMapObj, string>,
+        [formInput, inputValue]: [string, FormDataEntryValue]
+      ) => {
+        //form input values are all strings
+        mapObj.set(formInput as keyof Chars, inputValue as string);
+
+        return mapObj;
+      },
+      new Map()
+    );
+
+    setFormMapObj(formMap);
+    populatePasswordAndSetState(getRandKey);
   }
 
   return (
     <div
       // style={{ backgroundImage: gradientBG }}
-      className="my-8 mx-auto flex h-[700px] w-full flex-col gap-y-8 px-4 py-4 outline-dashed sm:w-[62%] md:h-[800px] md:w-[38%]"
+      className="my-8 mx-auto flex h-[700px] w-full flex-col gap-y-8 px-4 py-4 outline-dashed sm:my-14 sm:w-[62%] md:h-[800px] md:w-[38%]"
     >
       {/* title */}
       <h1 className="mx-auto h-[5%] text-3xl font-bold text-slate-400 outline-dotted">
@@ -26,8 +227,8 @@ const Home: NextPage = () => {
 
       {/* password output */}
       <div className="flex h-[10%] w-full flex-row items-center justify-between bg-myLightGrey px-4 text-gray-100 ">
-        <p className="sm:text-xl md:text-2xl">password</p>
-        <AiOutlineCopy className="scale-150 text-myTeal" />
+        <p className="overflow-ellipsis sm:text-xl md:text-2xl">password</p>
+        <AiOutlineCopy className="scale-150 text-myTeal hover:cursor-pointer" />
       </div>
 
       {/* options */}
@@ -37,11 +238,12 @@ const Home: NextPage = () => {
           <p className="text-gray-100 sm:text-xl md:text-2xl">
             Character Length
           </p>
-          <p className="text-myTeal sm:text-xl md:text-2xl">10</p>
+          <p className="text-myTeal sm:text-xl md:text-2xl">{charLength}</p>
         </div>
         {/* slider */}
         <form
           action="#"
+          id="passGenForm"
           onSubmit={handleFormSubmit}
           className="row-span-5 flex w-full flex-col items-start justify-around bg-myLightGrey p-4 text-gray-100 outline-dotted"
         >
@@ -53,6 +255,8 @@ const Home: NextPage = () => {
               id="charLength"
               min="8"
               max="32"
+              value={charLength}
+              onChange={(event) => setCharLength(event.currentTarget.value)}
               // className="form-range h-2 w-full appearance-none bg-myTeal p-0 focus:shadow-none focus:outline-none focus:ring-0 "
               className="w-full"
             />
@@ -85,22 +289,22 @@ const Home: NextPage = () => {
           </div>
 
           <div className="flex w-full flex-row">
-            <label htmlFor="numbers"></label>
+            <label htmlFor="number"></label>
             <input
               type="checkbox"
-              name="numbers"
-              id="numbers"
+              name="number"
+              id="number"
               className="sm:scale-125 md:scale-150"
             />
             <p className="ml-4 sm:text-xl md:text-2xl">Include Numbers</p>
           </div>
 
           <div className="flex w-full flex-row">
-            <label htmlFor="symbols"></label>
+            <label htmlFor="symbol"></label>
             <input
               type="checkbox"
-              name="symbols"
-              id="symbols"
+              name="symbol"
+              id="symbol"
               className="sm:scale-125 md:scale-150"
             />
             <p className="ml-4 sm:text-xl md:text-2xl">Include Symbols</p>
